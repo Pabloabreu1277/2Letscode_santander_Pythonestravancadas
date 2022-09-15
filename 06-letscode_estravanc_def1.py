1c674a578158c748ef536bd16a3648fe556c7e27
PabloAbreu <108098406+pabloloko1277@users.noreply.github.com>
2022-09-14 22:01:05 -0300

    Add files via upload

---
+++ 06-letscode_estravanc_def1.py
@@ -0,0 +1,21 @@
+def hello():
+    print('ola cambada')
+
+
+hello()
+
+
+def calmedia(valor1, valor2, valor3):
+    soma = valor1+ valor2+ valor3
+    media = soma / 3
+    return media
+
+resultado = calmedia(8,9,15)
+print(resultado)
+
+resultado2 = calmedia(valor1=8, valor2=9, valor3=15 )
+print(resultado2)
+
+print('ola', end=' ')
+print(', pietro')
+
